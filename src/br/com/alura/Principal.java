5efc89d001525c0fcddb0bf9f544492f4ddcfdb7
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-27 20:02:47 -0300

    commit aula de Strings e Regex 2/2

--- src/br/com/alura/Principal.java
+++ src/br/com/alura/Principal.java
@@ -15,5 +15,20 @@ public class Principal {
         if (matcher.find()) {
             System.out.println(matcher.group());
         }
+
+        System.out.println(formatarTelefone("2199887744"));
     }
-}
+
+        public static String formatarTelefone(String telefone) {
+            String regex = "(\\d{2})(\\d{4,5})(\\d{4})";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(telefone);
+
+            if (matcher.matches()) {
+                return String.format("(%s) %s-%s", matcher.group(1), matcher.group(2), matcher.group(3));
+            }
+
+            return "Número de telefone inválido!";
+        }
+    }
+
