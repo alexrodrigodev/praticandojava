880dc98ede1c480f37e7671f936cd36fc2ff8a06
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-27 19:53:33 -0300

    commit aula de Strings e Regex 1/2

--- src/br/com/alura/Principal.java
+++ src/br/com/alura/Principal.java
@@ -1,7 +1,19 @@
 package br.com.alura;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Principal {
     static void main(String[] args) {
 
+
+        String texto = "Meu email é jacqueline@gmail.com";
+        Pattern pattern = Pattern.compile("\\w+@\\w+.\\w+");
+        Matcher matcher = pattern.matcher(texto);
+
+
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+        }
     }
 }
